02cdb8132bb8dccb68d6692accc78ba55c283fe7
Allwyn46 <allwyn153@gmail.com>
2026-04-24 20:06:18 +0530

    added navbar component and completed basic structure and added prettierrc file

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="flex justify-between items-center">
+                <div>
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
